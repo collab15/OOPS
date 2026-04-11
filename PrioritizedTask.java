391390543764e7643e1355287660d1dcf500b2cd
b1lquees <jumanibilquees81@gmail.com>
2026-04-11 23:19:30 +0500

    Update PrioritizedTask.java

--- PrioritizedTask.java
+++ PrioritizedTask.java
@@ -1,10 +1,17 @@
-class PrioritizedTask {
+public class PrioritizedTask {
 
-    double priority;
-    Task task;
+    private double priority;
+    private Task task;
 
-    PrioritizedTask(double priority, Task task) {
+    public PrioritizedTask(double priority, Task task) {
         this.priority = priority;
         this.task = task;
+    }
+
+    public double getPriority() {
+        return priority;
+    }
+    public Task getTask() { 
+        return task; 
     }
 }
