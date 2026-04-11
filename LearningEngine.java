4a1e7d8e6be599c162c541e6ee7bd3cc867960be
b1lquees <jumanibilquees81@gmail.com>
2026-04-11 23:30:22 +0500

    Update LearningEngine.java

--- LearningEngine.java
+++ LearningEngine.java
@@ -1,32 +1,30 @@
 import java.util.List;
 
 public class LearningEngine {
+
     private double learningFactor;
     private int extentOfBacklogToLearnFrom;
 
-    LearningEngine(double learningFactor, int extentOfBacklogToLearnFrom){
+    public LearningEngine(double learningFactor, int extentOfBacklogToLearnFrom){
         this.learningFactor = learningFactor;
         this.extentOfBacklogToLearnFrom = extentOfBacklogToLearnFrom;
     }
 
     public Delta calculateCumulativeDelta(){
-
         List<Delta> deltabacklog = TaskSelectionHistory.getDeltabacklog(extentOfBacklogToLearnFrom);
-
         if (deltabacklog.isEmpty()) {
             return new Delta(0,0,0,0);
         }
-
         double d_importance = 0;
         double d_urgency = 0;
         double d_effort = 0;
         double d_length = 0;
 
         for(Delta deltalog : deltabacklog){
-            d_importance += deltalog.d_importance;
-            d_urgency += deltalog.d_urgency;
-            d_effort += deltalog.d_effort;
-            d_length += deltalog.d_length;
+            d_importance += deltalog.getImportance(); 
+            d_urgency += deltalog.getUrgency();
+            d_effort += deltalog.getEffort();
+            d_length += deltalog.getLength();
         }
 
         d_importance *= learningFactor;
@@ -34,10 +32,6 @@ public class LearningEngine {
         d_effort *= learningFactor;
         d_length *= learningFactor;
 
-        Delta cumulativeDelta = new Delta(d_importance, d_urgency, d_effort, d_length);
-
-        return cumulativeDelta;
-
+        return new Delta(d_importance, d_urgency, d_effort, d_length);
     }
-
 }
