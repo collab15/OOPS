7c0a128722d5577d5207ebfe2ef5a0209ad0b871
b1lquees <jumanibilquees81@gmail.com>
2026-04-18 00:50:08 +0500

    Update TaskSelectionHistory.java

--- TaskSelectionHistory.java
+++ TaskSelectionHistory.java
@@ -1,22 +1,34 @@
+package project;
+
 import java.util.List;
 import java.util.ArrayList;
+//import java.util.Collections;
 
-public abstract class TaskSelectionHistory {
+// abstract removed bec u cant create objects anyway everything is static
+public class TaskSelectionHistory {
+    // stores all the deltas
+    private static List<Delta> deltas= new ArrayList<>();
 
-    static List<Delta> deltas= new ArrayList<>();
+    public static void addDelta(Delta delta) {
+        deltas.add(delta);
+    }
 
-    static List<Delta> getDeltabacklog(int n){
+    // gets back n number of deltas from the backlog starting from the most recent one
+    private static List<Delta> getDeltaBacklog(int n){
+
+        if (n <= 0) return new ArrayList<>();
 
         List<Delta> last_n_Deltas = new ArrayList<>();
-        
+
         int size = deltas.size();
         int start = Math.max(0, size - n);
 
         for (int i = size - 1; i >= start; i--) {
             last_n_Deltas.add(deltas.get(i));
         }
+        // should we reverse the list to have the most recent delta at the end of the list???
+        //Collections.reverse(last_n_Deltas); // this is just for display 
 
-        return last_n_Deltas;
+        return new ArrayList<>(last_n_Deltas); // instead of return last_n_Deltas; bec outside code can modify history result 
     }
-    
 }
