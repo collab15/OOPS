ea32edc8bd5961efe087507207029ec5235119f2
b1lquees <jumanibilquees81@gmail.com>
2026-04-18 00:39:54 +0500

    Update LearningEngine.java

--- LearningEngine.java
+++ LearningEngine.java
@@ -1,9 +1,11 @@
+package project;
+
 import java.util.List;
 
 public class LearningEngine {
 
-    private double learningFactor;
-    private int extentOfBacklogToLearnFrom;
+    private double learningFactor; // controls how strong the learning is ex slow(0.1) full (1.0) agressive (x>1)
+    private int extentOfBacklogToLearnFrom; // how many past task selections to learn from
 
     public LearningEngine(double learningFactor, int extentOfBacklogToLearnFrom){
         this.learningFactor = learningFactor;
@@ -13,18 +15,20 @@ public class LearningEngine {
     public Delta calculateCumulativeDelta(){
         List<Delta> deltabacklog = TaskSelectionHistory.getDeltabacklog(extentOfBacklogToLearnFrom);
         if (deltabacklog.isEmpty()) {
-            return new Delta(0,0,0,0);
+            return new Delta(0,0,0,0); // if no past data no learning return zero delta
         }
         double d_importance = 0;
         double d_urgency = 0;
         double d_effort = 0;
         double d_length = 0;
 
+        //  going thru each past learning event in the backlog and summing it to get a cumulative delta
+        //  which represents the overall learning from the past selections
         for(Delta deltalog : deltabacklog){
             d_importance += deltalog.getImportance(); 
-            d_urgency += deltalog.getUrgency();
-            d_effort += deltalog.getEffort();
-            d_length += deltalog.getLength();
+            d_urgency    += deltalog.getUrgency();
+            d_effort     += deltalog.getEffort();
+            d_length     += deltalog.getLength();
         }
 
         d_importance *= learningFactor;
