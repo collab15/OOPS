3e6acc7f95eed0a326837ffa582cc4517a5c23fc
b1lquees <jumanibilquees81@gmail.com>
2026-04-18 00:48:33 +0500

    Update PrioritizedTask.java

--- PrioritizedTask.java
+++ PrioritizedTask.java
@@ -1,9 +1,12 @@
+package project;
+
 public class PrioritizedTask {
 
     private double priority;
     private Task task;
 
     public PrioritizedTask(double priority, Task task) {
+        if (task == null) throw new IllegalArgumentException("Task cannot be null");
         this.priority = priority;
         this.task = task;
     }
@@ -11,7 +14,16 @@ public class PrioritizedTask {
     public double getPriority() {
         return priority;
     }
+
+    // add setter if AI will recalculate scores dynamically
     public Task getTask() { 
         return task; 
     }
+
+
+    // need to add a comparing method
+    /*@Override
+    public int compareTo(PrioritizedTask other) {
+        return Double.compare(other.priority, this.priority);
+    }*/
 }
