b7038eb9d7b3c75b8a514d08593e6944bc4e624e
b1lquees <jumanibilquees81@gmail.com>
2026-04-18 00:39:26 +0500

    Update HeuristicEngine.java

--- HeuristicEngine.java
+++ HeuristicEngine.java
@@ -1,27 +1,31 @@
+package project;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+// responsible for selecting the best tasks to suggest to the user based on the current weights and pending tasks
 public class HeuristicEngine {
 
-    private int numberOfTasksToSuggest;
-    private Weights weights;
+    private int numberOfTasksToSuggest; // how many tasks to return 
+    private Weights weights; // used to calc priority score 
 
     public HeuristicEngine(int numberOfTasksToSuggest, Weights weights) {
         this.numberOfTasksToSuggest = numberOfTasksToSuggest;
         this.weights = weights;
     }
-
+// first you get pendiing tasks from TaskManager next you calculate a priority score
+//  for each task using the weights , then you sort the tasks by priority and return N tasks based on numberOfTasksToSuggest
     public List<Task> suggestTasks() {
-        List<Task> pendingTasks = TaskManager.getPendingTasks();
+        List<Task> pendingTasks = TaskManager.getPendingTasks(); // fetch all incomplete tasks uses TaskManager 
         List<PrioritizedTask> priorityList = prioritizeTasks(pendingTasks);
         return chooseHighPriorityTasks(numberOfTasksToSuggest, priorityList);
     }
 
-    private List<PrioritizedTask> prioritizeTasks(List<Task> pendingTasks) {
+    private List<PrioritizedTask> prioritizeTasks(List<Task> pendingTasks) { // assigns a score to each task
         List<PrioritizedTask> priorityList = new ArrayList<>();
         for (Task pendingTask: pendingTasks) {
-            double priority = weights.calculateScore(pendingTask);
+            double priority = weights.calculateScore(pendingTask); // use weights to compute score 
             PrioritizedTask prioritizedTask = new PrioritizedTask(priority, pendingTask);
             priorityList.add(prioritizedTask);
         }
@@ -35,7 +39,7 @@ public class HeuristicEngine {
         for (int i = 0; i < limit; i++) {
             highPriorityTasks.add(tasksSortedByPriority.get(i).getTask());
         }
-        return highPriorityTasks;
+        return highPriorityTasks; 
     }
 
     private List<PrioritizedTask> sortByPriority(List<PrioritizedTask> priorityList) {
