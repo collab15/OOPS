492d8499ea368b51e11a75c2d38a3011b9c26a12
b1lquees <jumanibilquees81@gmail.com>
2026-04-18 19:02:57 +0500

    Update SessionManager.java

--- SessionManager.java
+++ SessionManager.java
@@ -8,7 +8,7 @@ public class SessionManager implements TimerListener {
 
     public SessionManager(TaskManager tm) {
         this.taskManager = tm;
-        this.timer = new PomodoroTimer(this);
+        this.timer = new PomodoroTimer(this); // passes this so SessionManager listens to timer events
     }
     public Task getCurrentTask() {
         return currentTask;
@@ -19,38 +19,39 @@ public class SessionManager implements TimerListener {
     }
     
     public void startSession(Task task) {
-        if (task == null) return;
+        if (task == null) // do nothing if no task
+            return;
 
         this.currentTask = task;
         this.sessionActive = true;
-        timer.start(task);
+        timer.start(task); // starting the timer in background thread
     }
 
     public void stopSession() {
-
-        if (!sessionActive || currentTask == null) return;
+    // if there is no session running do nothing
+        if (!sessionActive || currentTask == null)
+            return;
         sessionActive = false;
-        // stop timer safely
+        // stop timer safely interrupt the thread
         timer.stop(currentTask);
     }
 
     @Override
     public void onTimerComplete(Task task) {
 
-        sessionActive = false;
-
+        sessionActive = false; // session finished
         System.out.println("Task completed: " + task.getName());
 
-        taskManager.completeTask(task);
+        taskManager.completeTask(task); // this will mark task as completed in TaskManager
         currentTask = null;
     }
 
-    @Override
+    @Override // if timer is stopped early or interrupted 
     public void onTimerInterrupted(Task task, int timeRemaining) {
 
         sessionActive = false;
         System.out.println("Task interrupted: " + task.getName());
-        task.reduceLength(timeRemaining);
+        task.reduceLength(timeRemaining); /// this saves remaining time left for task
         currentTask = null;
     }
 
