e017d74f849dddf2a46a3271d3613e4242d93a60
b1lquees <jumanibilquees81@gmail.com>
2026-04-21 13:09:02 +0500

    Update CompletedTasksViewMenu.java

--- CompletedTasksViewMenu.java
+++ CompletedTasksViewMenu.java
@@ -1,32 +1,43 @@
 package project;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CompletedTasksViewMenu extends Menu {
 
-    public CompletedTasksViewMenu() {
-        setMenuSelections();
-        setMenuItems(TaskManager.getCompletedTasks()); // loads completed tasks from TaskManager into  menuItems
+    private final TaskManager taskManager;
+
+    public CompletedTasksViewMenu(TaskManager taskManager) {
+        this.taskManager = taskManager;
     }
 
     @Override
     void setMenuSelections() {
-        this.menuSelections = List.of(
-            "Back" // only back bec u r only viewing completed tasks 
-        );
-    }
 
-    @Override
-    void handleSelection() {
-     // arrows
-    }
+        List<Task> tasks = taskManager.getCompletedTasks();
+        setMenuItems(tasks);
 
-    @Override
-    void select() {
-        switch (currentIndex) {
-            case 0:
-               // go back 
-                break;
+        List<String> selections = new ArrayList<>();
+
+        for (Task task : tasks) {
+            selections.add(task.getName());
         }
+
+        selections.add("Back");
+
+        this.menuSelections = selections;
+    }
+
+    @Override
+    void handleSelection(int index) {
+
+        //  set shouldExit so the display loop actually exits on Back
+        if (index == menuSelections.size() - 1) {
+            shouldExit = true;
+            return;
+        }
+
+        Task selectedTask = menuItems.get(index);
+        System.out.println("Viewing completed task: " + selectedTask.getName());
     }
 }
