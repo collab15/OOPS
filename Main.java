02931618b4f41cda7c97cc11c1c076d0a8e59755
b1lquees <jumanibilquees81@gmail.com>
2026-04-21 13:10:39 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,31 @@
-public class Main {
+package project;
 
+public class Main {
     public static void main(String[] args) {
 
         Settings.loadSettings();
 
+        // Load tasks from storage first
+        TaskManager.loadTasks();
+
+        TaskSelectionHistory history = new TaskSelectionHistory();
+
+        LearningEngine learning = new LearningEngine(
+                Settings.AI_Settings.getLearningFactor(),
+                Settings.AI_Settings.getBacklogSize(),
+                history
+        );
+
+        Weights weights = new Weights(1.0, 1.0, 1.0, 1.0);
+
         TaskManager taskManager = new TaskManager();
+        // AIEngine now stored so it can be used
+        AIEngine ai = new AIEngine(weights, learning, taskManager);
 
-        Weights weights = new Weights(5, 8, -3, -2);
+        // Session system
+        SessionManager sessionManager = new SessionManager(taskManager);
 
-        LearningEngine learning = new LearningEngine( Settings.AI_Settings.getLearningFactor(), Settings.AI_Settings.getBacklogSize() );
-
-        AIEngine ai = new AIEngine(weights, learning);
-
-        List<Task> suggestedTasks = ai.suggestTasks(taskManager.getPendingTasks(), Settings.UserSettings.getSuggestionCount());
-
-        MainMenu menu = new MainMenu();
-        menu.setMenuItems(suggestedTasks);
-        menu.handleSelection();
+        // Start UI
+        new MainMenu(taskManager, sessionManager).display();
     }
 }
