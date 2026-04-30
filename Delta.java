5b85f13626adbd9467a96f7b26d04ee5b97e277e
b1lquees <jumanibilquees81@gmail.com>
2026-04-30 20:34:23 +0500

    Update Delta.java

--- Delta.java
+++ Delta.java
@@ -1,4 +1,7 @@
-public class Delta {
+import java.io.Serializable;
+public class Delta implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private double importance;
     private double urgency;
@@ -17,7 +20,7 @@ public class Delta {
     // =========================
     public static Delta fromTaskComparison(Task selected, Task suggested) {
 
-        // simple heuristic difference model (you can improve later)
+        // simple heuristic difference model
         double importance = selected.getImportance() - suggested.getImportance();
         double urgency    = selected.getUrgency()    - suggested.getUrgency();
         double effort     = suggested.getEffort()    - selected.getEffort();
